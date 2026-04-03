422e6798ed1bd3ae8c2025269142aa9215a78125
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-04-03 16:41:15 -0400

    added image bundling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import "./styles.css";
+
+import odinImage from "./odin.png";
+const image = document.createElement("img");
+image.src = odinImage;
+document.body.appendChild(image);
+
 import { greeting } from "./greeting.js";
 
 console.log(greeting);
